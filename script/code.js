aa4decbaa4f5e59e719005578bce878e24844b90
Arif2ankhi <arif2ankhi@gmail.com>
2024-10-06 23:58:43 +0100

    Created js file for category

--- script/code.js
+++ script/code.js
@@ -1,1 +1,41 @@
 console.log('coding');
+
+//  categories 
+
+// create categories
+const loadCategories = () => {
+    // fetch the data
+    fetch('https://openapi.programming-hero.com/api/peddy/categories')
+    .then((res)=>res.json())
+    .then((data)=> displayCategories(data.categories))
+    .catch((error) => console.log(error));
+}
+
+// {
+//     {
+//         "id": 1,
+//         "category": "Cat",
+//         "category_icon": "https://i.ibb.co.com/N7dM2K1/cat.png"
+//     }
+// }
+
+// display categories
+const displayCategories = (categories) => {
+    const categoryContainer = document.getElementById('categories');
+    categories.forEach(item => {
+        console.log(item);
+        // create a button
+        const button = document.createElement('button');
+        button.classList = 'btn';
+        button.innerHTML =item.category;
+
+        // add button to category container
+        categoryContainer.append(button);
+        
+    });
+};
+
+
+
+
+loadCategories();
